297e4bce2f0fea1251d8fe4ab450619a38bcd98c
kitsuneMori <2794637787@qq.com>
2025-12-15 22:00:20 +0800

    feat(girlFriend): 添加Phone类并实现相关功能
    在girlFriend.java中添加了一个新的类Phone，该类用于表示手机对象，并实现了以下功能：
    - 添加了三个私有成员变量brand、color和price，分别表示手机的品牌、颜色和价格。
    - 实现了show方法，用于打印手机的品牌、颜色和价格。
    - 实现了两个构造方法，一个无参构造方法和一个包含品牌、颜色和价格参数的构造方法。
    - 实现了setBrand、setColor和setPrice方法，用于设置手机的品牌、颜色和价格。
    - 添加了sendMessage方法，用于模拟发送信息的行为，并打印相关信息。
    - 添加了call方法，用于模拟打电话的行为，并打印相关信息。
    
    同时，将Test类中的main方法移到了Phone类的实现之后，以保持代码的整洁。

--- day05-面向对象编程/作业/girlFriend.java
+++ day05-面向对象编程/作业/girlFriend.java
@@ -6,17 +6,6 @@ public class girlFriend {
     
 }
 
-class Test {
-    public static void main(String[] args) {
-        Teacher t1=new Teacher();
-        t1.setName("杨老师");
-        t1.setWorkId(001);
-        Teacher t2=new Teacher("李老师",002);
-        t1.show();
-        t2.show();
-    }
-}
-
 class Student{
     private String name;
     private int age;
@@ -76,4 +65,56 @@ class Teacher{
         return workId;
     }
 
+}
+
+class Phone{
+    private String brand;
+    private String color;
+    private int price;
+
+    public void show(){
+        System.out.println("品牌"+brand+" 颜色"+color+" 价格"+price);
+    }
+
+    public Phone(){
+
+    }
+
+    public Phone(String brand,String color,int price){
+        this.brand = brand;
+        this.color = color;
+        this.price = price;
+    }
+
+    public void setBrand(String brand){ // 设置品牌
+        this.brand = brand;
+    }
+    
+    public void setColor(String color){
+        this.color = color;
+    }
+
+    public void setPrice(int price){ // 设置价格
+        this.price = price;
+    }
+    
+    public void sendMessage(){
+        System.out.println("正在发送信息,使用的是   "+brand+"  "+color+"  "+price);
+    }
+
+    public void call(){
+        System.out.println("正在打电话,使用的是   "+brand+"  "+color+"  "+price);
+    }
+
+}
+
+class Test {
+    public static void main(String[] args) {
+        Teacher t1=new Teacher();
+        t1.setName("杨老师");
+        t1.setWorkId(001);
+        Teacher t2=new Teacher("李老师",002);
+        t1.show();
+        t2.show();
+    }
 }
