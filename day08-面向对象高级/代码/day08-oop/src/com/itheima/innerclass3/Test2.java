48ab3dbe99565287605268e3cd096768907459e1
kitsuneMori <2794637787@qq.com>
2025-12-21 21:59:29 +0800

    refactor(oop): 使用Lambda表达式替换匿名内部类简化代码
    - 用Lambda表达式替换Test2中匿名内部类实现，简化游泳比赛示例代码
    - 用Lambda表达式替换Test4中匿名内部类Comparator，实现学生年龄升序排序
    - 优化代码结构，删除未使用的导入语句
    - 修改项目配置文件，添加lombok依赖库支持

--- day08-面向对象高级/代码/day08-oop/src/com/itheima/innerclass3/Test2.java
+++ day08-面向对象高级/代码/day08-oop/src/com/itheima/innerclass3/Test2.java
@@ -4,22 +4,12 @@ public class Test2 {
     public static void main(String[] args) {
         // 目标：搞清楚匿名内部类的使用形式（语法）： 通常可以做为一个对象参数传输给方法使用。
         // 需求：学生，老师都要参加游泳比赛。
-        Swim s1 = new Swim() {
-            @Override
-            public void swimming() {
-                System.out.println("学生🏊‍贼快~~~~");
-            }
-        };
+        Swim s1 = () -> System.out.println("学生🏊‍贼快~~~~");
         start(s1);
 
         System.out.println("====================================");
 
-        start(new Swim() {
-            @Override
-            public void swimming() {
-                System.out.println("老师🏊‍贼溜~~~~");
-            }
-        });
+        start(() -> System.out.println("老师🏊‍贼溜~~~~"));
     }
 
     // 设计一个方法，可以接收老师，和学生开始比赛。
--- day08-面向对象高级/代码/day08-oop/src/com/itheima/innerclass3/Test4.java
+++ day08-面向对象高级/代码/day08-oop/src/com/itheima/innerclass3/Test4.java
@@ -1,7 +1,6 @@
 package com.itheima.innerclass3;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 public class Test4 {
     public static void main(String[] args) {
@@ -20,27 +19,11 @@ public class Test4 {
         //           参数一： 需要排序的数组
         //           参数二： 需要给sort方法声明一个Comparator比较器对象（指定排序的规则）
         //    sort方法内部会调用匿名内部类对象的compare方法，对数组中的学生对象进行两两比较，从而实现排序。
-        Arrays.sort(students, new Comparator<Student>() {
-            @Override
-            public int compare(Student o1, Student o2) {
-                // 指定排序规则：
-                // 如果你认为左边对象 大于 右边对象 那么返回正整数。
-                // 如果你认为左边对象 小于 右边对象 那么返回负整数。
-                // 如果两边相等那么返回0
-//                if(o1.getAge() > o2.getAge()){
-//                    return 1;
-//                }else if(o1.getAge() < o2.getAge()){
-//                      return -1;
-//                }
-//                return 0;
-//                return o1.getAge() - o2.getAge(); // 按照年龄升序！
-                 return o2.getAge() - o1.getAge(); // 按照年龄降序！
-            }
-        });
+
+        Arrays.sort(students, (s1, s2) -> s1.getAge() - s2.getAge());
 
         // 遍历数组中的学生对象并输出
-        for (int i = 0; i < students.length; i++) {
-            Student s = students[i];
+        for (Student s : students) {
             System.out.println(s);
         }
     }
