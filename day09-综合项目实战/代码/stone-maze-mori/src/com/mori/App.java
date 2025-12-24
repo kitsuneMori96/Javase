dbae76a22b5b3f32b3474e19730c13f4ef115b61
kitsuneMori <2794637787@qq.com>
2025-12-24 22:14:32 +0800

    feat(app): 添加石头迷阵游戏主界面和核心逻辑
    - 创建主窗口MainFrame，初始化游戏界面和菜单
    - 实现棋盘元素位置数据初始化及打乱
    - 绑定键盘上下左右键实现棋盘拼图移动逻辑
    - 动态更新面板显示拼图图片和背景
    - 配置项目模块文件和资源路径设置
    - 新增App入口类启动游戏界面

---
+++ day09-综合项目实战/代码/stone-maze-mori/src/com/mori/App.java
@@ -0,0 +1,7 @@
+package com.mori;
+
+public class App {
+    public static void main(String[] args) {
+        new MainFrame();
+    }
+}
---
+++ day09-综合项目实战/代码/stone-maze-mori/src/com/mori/MainFrame.java
@@ -0,0 +1,196 @@
+package com.mori;
+
+import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+
+public class MainFrame extends JFrame {
+    private static final String IMAGE_PATH = "D:\\Download\\javaseaiproject\\day09-综合项目实战\\代码\\stone-maze-mori\\src\\image\\";
+    private int[][] data = new int[][]{
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 0}
+    };
+
+
+    public MainFrame() {
+        //初始化窗口
+        initWindow();
+        // 初始化数据
+        initData();
+        //初始化界面图片
+        initPanel();
+        //初始化菜单
+        initMenu();
+        //绑定按键事件
+        initKeyPressEvent();
+        //将窗口始终设置为焦点
+        this.setFocusable(true);
+        this.setVisible(true);
+    }
+
+    private void initKeyPressEvent() {
+        //绑定上下左右按键事件
+        this.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int keyCode = e.getKeyCode();
+                switch (keyCode) {
+                    case KeyEvent.VK_UP:
+                        dataMove(KeyEvent.VK_UP);
+                        break;
+                    case KeyEvent.VK_DOWN:
+                        dataMove(KeyEvent.VK_DOWN);
+                        break;
+                    case KeyEvent.VK_LEFT:
+                        dataMove(KeyEvent.VK_LEFT);
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        dataMove(KeyEvent.VK_RIGHT);
+                        break;
+                    default:
+                        break;
+                }
+                initPanel();
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
+    }
+
+    protected void dataMove(int vkRight) {
+        if(vkRight==KeyEvent.VK_UP) {
+            // 上移
+            for(int i=0;i<data.length;i++){
+                for(int j=0;j<data[i].length;j++){
+                    if(data[i][j]==0){
+                        if(i!=data.length-1){
+                            int temp=data[i][j];
+                            data[i][j]=data[i+1][j];
+                            data[i+1][j]=temp;
+                            System.out.println("上移成功");
+                            return;
+                        }
+                    }
+                }
+            }
+        } else if(vkRight==KeyEvent.VK_DOWN) {
+            // 下移
+            for(int i=0;i<data.length;i++){
+                for(int j=0;j<data[i].length;j++){
+                    if(data[i][j]==0){
+                        if(i!=0){
+                            int temp=data[i][j];
+                            data[i][j]=data[i-1][j];
+                            data[i-1][j]=temp;
+                            return;
+                        }
+                    }
+                }
+            }
+        } else if(vkRight==KeyEvent.VK_LEFT) {
+            // 左移
+            for(int i=0;i<data.length;i++){
+                for(int j=0;j<data[i].length;j++){
+                    if(data[i][j]==0){
+                        if(j!=data[i].length-1){
+                            int temp=data[i][j];
+                            data[i][j]=data[i][j+1];
+                            data[i][j+1]=temp;
+                            return;
+                        }
+                    }
+                }
+            }
+        } else if(vkRight==KeyEvent.VK_RIGHT) {
+            // 右移
+            for(int i=0;i<data.length;i++){
+                for(int j=0;j<data[i].length;j++){
+                    if(data[i][j]==0){
+                        if(j!=0){
+                            int temp=data[i][j];
+                            data[i][j]=data[i][j-1];
+                            data[i][j-1]=temp;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void initData() {
+        //打乱二维数组中的元素
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                int x = (int) (Math.random() * data.length );
+                int y = (int) (Math.random() * data[i].length );
+                int temp = data[i][j];
+                data[i][j] = data[x][y];
+                data[x][y] = temp;
+            }
+        }
+    }
+
+    public void initWindow() {
+        //初始化窗口
+        setTitle("石头迷阵");
+        setSize(465, 580);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public void initMenu() {
+        //初始化菜单
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menu = new JMenu("设置");
+        JMenuItem item1 = new JMenuItem("重启游戏");
+        JMenuItem item2 = new JMenuItem("退出");
+        menu.add(item1);
+        menu.add(item2);
+        menuBar.add(menu);
+        this.setJMenuBar(menuBar);
+        item1.addActionListener(e -> {
+            //重启游戏
+
+        });
+        item2.addActionListener(e -> {
+            //退出游戏
+
+        });
+    }
+    public void initPanel() {
+        // 初始化界面图片(先添加前景图片)
+        setLayout(null);
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[i].length; j++) {
+                JLabel imageLabel = new JLabel(new ImageIcon(IMAGE_PATH + data[i][j] + ".png"));
+                add(imageLabel);
+                imageLabel.setBounds(20+j * 100, 60+i * 100, 100, 100);
+            }
+        }
+
+        // 最后添加背景图（确保在最底层）
+        JLabel label = new JLabel(new ImageIcon(IMAGE_PATH + "background.png"));
+        add(label);
+        label.setBounds(0, 0, 450, 484);
+    }
+
+}
+
+class Application {
+    static void main(String[] args) {
+        new MainFrame();
+    }
+}
